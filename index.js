32ddbe748908e8b68d214f5e557275c629caae99
Chy Ihsan <43118152+Cihsan@users.noreply.github.com>
2023-08-28 20:16:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000
 const app = express()
 const jwt = require('jsonwebtoken');
 const { query } = require('express');
+app.use(express.urlencoded({ extended: true }))
+
 app.use(cors()) //
 app.use(express.json()) //for parse
 //task
